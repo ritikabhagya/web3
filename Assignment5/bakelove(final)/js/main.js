ef8ed94d847896a96c04f1aa370b962aabf9d09e
Ritika Bhagya <ritika.bhagya@gmail.com>
2013-10-23 21:23:25 -0400

    minor fixes

--- Assignment5/bakelove(final)/js/main.js
+++ Assignment5/bakelove(final)/js/main.js
@@ -16,16 +16,6 @@ function menuShows(){
 	console.log(menuContainerPosY);
 
 	if(scrollValueY > menuContainerPosY){
-		// console.log("you reached the menu div");
-
-		// var thm_delay1 = document.getElementsByClassName("thm_delay1").item(0);
-		// thm_delay1.classList.add("is-active");
-		// var thm_delay2 = document.getElementsByClassName("thm_delay2").item(0);
-		// thm_delay2.classList.add("is-active");
-		// var thm_delay3 = document.getElementsByClassName("thm_delay3").item(0);
-		// thm_delay3.classList.add("is-active");
-		// var thm_delay4 = document.getElementsByClassName("thm_delay4").item(0);
-		// thm_delay4.classList.add("is-active");
 
 		var menu_first_column = document.getElementsByClassName("thm_first_column").item(0);
 		menu_first_column.classList.add("is-active");
@@ -38,18 +28,6 @@ function menuShows(){
 	}
 }
 
-// $(function () {
- 
-// $('a[href^="#"]').click(function(event) {
-// var id = $(this).attr("href");
-// var offset = 20;
-// var target = $(id).offset().top - offset;
- 
-// $('html, body').animate({scrollTop:target}, 500);
-// event.preventDefault();
-// });
- 
-// });
 
 
 
