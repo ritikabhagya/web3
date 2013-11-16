4b20ab0e229efba4e2fc949e2c17f798c4bb554e
Ritika Bhagya <ritika.bhagya@gmail.com>
2013-11-16 18:46:37 -0500

    playing around with posts and forms

---
+++ Assignment7/discussions/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+
+<?php
+	mysql_connect('localhost', 'root', 'root');
+	mysql_select_db('Biodata');
+
+	$username = $_POST['username'];
+	$discussion = $_POST['discussion'];
+	$gender = $_POST['gender'];
+
+	$sql = "INSERT INTO discussion (username, discussion, gender) 
+	VALUES ('$username', '$discussion', '$gender')";
+	$result = mysql_query($sql);
+?>
+
+<html>
+<head>
+	<title>Discussions</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link href='http://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
+</head>
+
+	<body>
+		<h1>Add a discussion question</h1>
+
+		<div class="main">
+			<form method="post" id="nl-form" class="nl-form" />
+
+				I am a
+
+				<select required name="gender">
+					<option value="default" selected>Boy</option>
+					<option value="boy">Boy</option>
+					<option value="girl">Girl</option>
+					<option value="man">Man</option>
+					<option value="woman">Woman</option>
+				</select>
+				<br />
+
+				and my name is
+				<input type="text" name="username" placeholder="my name" required/>
+				<br />
+
+				Today I would like to talk about <input class="discussion" type="text" name="discussion" placeholder="how gay marriage should be made legal in India" required/>
+				
+				<div class="nl-submit-wrap">
+					<button class="nl-submit" type="submit" value="Submit" />Submit</button>
+				</div>
+
+				<div class="nl-overlay"></div>
+			</form>
+			
+			<p> <?php $query = mysql_query("SELECT username, discussion FROM discussion");
+			
+			while($row=mysql_fetch_array($query)){
+			?> <div class="row"> Name: <?php echo $row['username'];?><br>
+			Discussion: <?php echo $row['discussion']; ?>
+				<a href="#"><div class="join">Join discussion</div></a>
+			</div><br>
+			<?php 
+			}
+			?></p>
+
+		</div>
+	</body>
+</html>
+
+<?php mysql_close(); ?>
